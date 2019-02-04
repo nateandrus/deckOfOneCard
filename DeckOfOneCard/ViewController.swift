855bff0818640d7897724c3dd84ba10fbc579183
Nathan Andrus <nathanpandrus@gmail.com>
2019-02-04 10:39:59 -0700

    Initial Commit

---
+++ DeckOfOneCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeckOfOneCard
+//
+//  Created by Nathan Andrus on 2/4/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
